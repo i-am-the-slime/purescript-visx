d7218cbeb12463c530930215bb05fe8757fde125
Mark Eibes <mark.eibes@gmail.com>
2021-07-06 08:50:52 +0200

    Add BarRounded

--- src/VISX/Shape.js
+++ src/VISX/Shape.js
@@ -1,4 +1,5 @@
 const shape = require("@visx/shape")
 
 exports.barImpl = shape.Bar
+exports.barRoundedImpl = shape.BarRounded
 exports.linePathImpl = shape.LinePath
--- src/VISX/Shape.purs
+++ src/VISX/Shape.purs
@@ -1,26 +1,29 @@
-module VISX.Shape where
+module VISX.Shape (bar, barRounded, linePath) where
 
-import Prelude
 import Prim.Row (class Union)
 import React.Basic (ReactComponent)
 import React.Basic.Events (EventHandler)
 import VISX.Curve (CurveType)
 
-foreign import barImpl ∷ ∀ r. ReactComponent { | r }
+type BarPropsCommon r
+  = ( key ∷ String
+    , x ∷ Number
+    , y ∷ Number
+    , width ∷ Number
+    , height ∷ Number
+    , fill ∷ String
+    , onClick ∷ EventHandler
+    | r
+    )
 
-type BarProps =
-  ( key ∷ String
-  , x ∷ Number
-  , y ∷ Number
-  , width ∷ Number
-  , height ∷ Number
-  , fill ∷ String
-  , onClick ∷ EventHandler
-  )
+type BarProps
+  = BarPropsCommon ()
+
+foreign import barImpl ∷ ∀ r. ReactComponent { | r }
 
 bar ∷
   ∀ attrs attrs_.
-  Union attrs attrs_ BarProps =>
+  Union attrs attrs_ BarProps ⇒
   ReactComponent
     { width ∷ Number
     , height ∷ Number
@@ -30,30 +33,56 @@ bar ∷
     }
 bar = barImpl
 
+foreign import barRoundedImpl ∷ ∀ r. ReactComponent { | r }
+
+type BarRoundedProps
+  = BarPropsCommon ( all ∷ Int
+    , top ∷ Int
+    , right ∷ Int
+    , bottom ∷ Int
+    , left ∷ Int
+    , topRight ∷ Int
+    , bottomRight ∷ Int
+    , bottomLeft ∷ Int
+    , topLeft ∷ Int
+    )
+
+barRounded ∷
+  ∀ attrs attrs_.
+  Union attrs attrs_ BarRoundedProps ⇒
+  ReactComponent
+    { width ∷ Number
+    , height ∷ Number
+    , x ∷ Number
+    , y ∷ Number
+    | attrs
+    }
+barRounded = barRoundedImpl
+
 foreign import linePathImpl ∷ ∀ r. ReactComponent { | r }
 
-type LinePathProps a =
-  ( key ∷ String
-  , x ∷ Number
-  , y ∷ Number
-  , width ∷ Number
-  , height ∷ Number
-  , fill ∷ String
-  , onClick ∷ EventHandler
-  , curve ∷ CurveType
-  , data ∷ Array a
-  , stroke ∷ String
-  , strokeWidth ∷ Number
-  , strokeOpacity ∷ Number
-  , shapeRendering ∷ String
-  , markerMid ∷ String
-  , markerStart ∷ String
-  , markerEnd ∷ String
-  )
+type LinePathProps a
+  = ( key ∷ String
+    , x ∷ Number
+    , y ∷ Number
+    , width ∷ Number
+    , height ∷ Number
+    , fill ∷ String
+    , onClick ∷ EventHandler
+    , curve ∷ CurveType
+    , data ∷ Array a
+    , stroke ∷ String
+    , strokeWidth ∷ Number
+    , strokeOpacity ∷ Number
+    , shapeRendering ∷ String
+    , markerMid ∷ String
+    , markerStart ∷ String
+    , markerEnd ∷ String
+    )
 
 linePath ∷
   ∀ a attrs attrs_.
-  Union attrs attrs_ (LinePathProps a) =>
+  Union attrs attrs_ (LinePathProps a) ⇒
   ReactComponent
     { width ∷ Number
     , height ∷ Number
