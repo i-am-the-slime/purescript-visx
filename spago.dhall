ef6218ff16df8b6f82980fa9868ade039d7f4540
Mark Eibes <mark.eibes@gmail.com>
2021-07-27 13:35:02 +0200

    Add formatNumber and formatInt

--- spago.dhall
+++ spago.dhall
@@ -22,6 +22,7 @@ to generate this file without the comments in this block.
   , "foreign"
   , "foreign-object"
   , "functions"
+  , "integers"
   , "justifill"
   , "lists"
   , "maybe"
--- src/D3/Format.purs
+++ src/D3/Format.purs
@@ -1,5 +1,18 @@
 module D3.Format where
 
+import Prelude
+import Unsafe.Coerce (unsafeCoerce)
+import Effect (Effect)
+import Data.Int (toNumber)
+
 foreign import data D3Format ∷ Type
 
-foreign import format ∷ String -> D3Format
+foreign import format ∷ String → D3Format
+
+formatNumber ∷ D3Format → Number → Effect String
+formatNumber formatFn n =
+  pure $ ((unsafeCoerce formatFn) ∷ Number → String) n
+
+formatInt ∷ D3Format → Int → Effect String
+formatInt formatFn i =
+  pure $ ((unsafeCoerce formatFn) ∷ Number → String) (toNumber i)
