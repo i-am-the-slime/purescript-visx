5ea6bd72f874a7c51309e24f34fb6ff223d1b1c9
Mark Eibes <mark.eibes@gmail.com>
2021-08-31 10:38:44 +0200

    Guess the function

--- src/D3/TimeFormat.js
+++ src/D3/TimeFormat.js
@@ -2,6 +2,6 @@ const d3TimeFormat = require("d3-time-format");
 
 exports.timeFormat = d3TimeFormat.timeFormat;
 
-exports.localeTimeFormat = (str) => (locale) => locale.timeFormat(str);
+exports.localeTimeFormat = (str) => (locale) => locale.format(str);
 
 exports.timeFormatLocale = (def) => d3TimeFormat.timeFormatLocale(def);
