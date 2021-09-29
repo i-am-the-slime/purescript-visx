2d27142849462e63d190bd08bdd54efffc65b976
Mark Eibes <mark.eibes@gmail.com>
2021-09-29 11:41:06 +0200

    Hat tip

--- src/VISX/Tooltip.purs
+++ src/VISX/Tooltip.purs
@@ -49,20 +49,19 @@ useTooltip =
         , updateTooltip: baseProps.updateTooltip # runEffectFn1
         }
 
--- useTooltipInPortal ∷ ∀ a. Hook (UseTooltip a) (Maybe (UseTooltipValues a))
--- useTooltipInPortal =
---   unsafeHook useTooltipInPortalImpl
---     <#> \baseProps →
---         sequenceRecord
---           $
---             { hideTooltip: uorToMaybe baseProps.hideTooltip
---             , showTooltip: uorToMaybe baseProps.showTooltip <#> runEffectFn1
---             , tooltipData: uorToMaybe baseProps.tooltipData
---             , tooltipLeft: uorToMaybe baseProps.tooltipLeft
---             , tooltipOpen: uorToMaybe baseProps.tooltipOpen
---             , tooltipTop: uorToMaybe baseProps.tooltipTop
---             , updateTooltip: uorToMaybe baseProps.updateTooltip <#> runEffectFn1
---             }
+useTooltipInPortal ∷ ∀ a. Hook (UseTooltip a) (UseTooltipValues a)
+useTooltipInPortal =
+  unsafeHook useTooltipInPortalImpl
+    <#> \baseProps →
+        { hideTooltip: baseProps.hideTooltip
+        , showTooltip: baseProps.showTooltip # runEffectFn1
+        , tooltipData: uorToMaybe baseProps.tooltipData
+        , tooltipLeft: uorToMaybe baseProps.tooltipLeft
+        , tooltipOpen: baseProps.tooltipOpen
+        , tooltipTop: uorToMaybe baseProps.tooltipTop
+        , updateTooltip: baseProps.updateTooltip # runEffectFn1
+        }
+
 foreign import tooltipImpl ∷ ∀ p. ReactComponent p
 foreign import defaultStylesImpl ∷ Foreign
 foreign import tooltipWithBoundsImpl ∷ ∀ p. ReactComponent p
