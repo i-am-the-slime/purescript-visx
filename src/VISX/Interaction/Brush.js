a0c24d3f9e8e1a1e869208f723ea87f70be99372
Mark Eibes <mark.eibes@gmail.com>
2021-09-30 14:06:36 +0200

    These were just typescript types

--- src/VISX/Interaction/Brush.js
+++ src/VISX/Interaction/Brush.js
@@ -1,7 +1,3 @@
 const brush = require("@visx/brush");
-const baseBrush = require("@visx/brush/lib/BaseBrush");
 
 exports.brushImpl = brush.Brush;
-exports.baseBrushImpl = baseBrush.default;
-exports.baseBrushStateImpl = baseBrush.BaseBrushState;
-exports.baseBrushUpdateImpl = baseBrush.BaseBrushUpdate;
--- src/VISX/Interaction/Brush.purs
+++ src/VISX/Interaction/Brush.purs
@@ -3,7 +3,3 @@ module VISX.Interaction.Brush where
 import React.Basic (ReactComponent)
 
 foreign import brushImpl ∷ ∀ a. ReactComponent a
-
-foreign import baseBrushImpl ∷ ∀ a. ReactComponent a
-foreign import baseBrushStateImpl ∷ ∀ a. ReactComponent a
-foreign import baseBrushUpdateImpl ∷ ∀ a. ReactComponent a
