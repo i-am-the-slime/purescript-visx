03b7d7005185dda9e082ead63e07bdbe47953fad
Mark Eibes <mark.eibes@gmail.com>
2022-01-04 09:37:42 +0100

    Oopsi

--- src/VISX/Util/SVG/Pattern.js
+++ src/VISX/Util/SVG/Pattern.js
@@ -1,8 +1,8 @@
 import { pattern as _pattern, PatternLines, PatternCircles, PatternWaves, PatternHexagons, PatternPath} from "@visx/pattern"
 
-exports.pattern = _pattern
-exports.patternLinesImpl = PatternLines
-exports.patternCirclesImpl = PatternCircles
-exports.patternWavesImpl = PatternWaves
-exports.patternHexagonsImpl = PatternHexagons
-exports.patternPathImpl = PatternPath
+export const pattern = _pattern
+export const patternLinesImpl = PatternLines
+export const patternCirclesImpl = PatternCircles
+export const patternWavesImpl = PatternWaves
+export const patternHexagonsImpl = PatternHexagons
+export const patternPathImpl = PatternPath
